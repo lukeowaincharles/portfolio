6c46d945a378b0a00adb5cb993c02b9b742ec8ed
Luke <lukeowaincharles@gmail.com>
2018-12-11 13:05:41 +0000

    add smooth scrolling function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-    
+
+    // smooth scroll function
+    $('a[href^="#"]').on('click', function (e) {
+
+        var target = $(this.getAttribute('href'));
+
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
 });
