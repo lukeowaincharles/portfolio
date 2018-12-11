094ab11fe6d49b208f1bb0dca3a3a39d99fffe95
Luke <lukeowaincharles@gmail.com>
2018-12-11 13:07:23 +0000

    add compiled dist files

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -198,5 +198,18 @@ var broozr = function (options) {
 broozr();
 
 $(document).ready(function () {
-    
+
+    // smooth scroll function
+    $('a[href^="#"]').on('click', function (e) {
+
+        var target = $(this.getAttribute('href'));
+
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
 });
